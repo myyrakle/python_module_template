2f882edb456addd588dd034972e38f7ab3e9a44f
myyrakle <sssang97@naver.com>
2022-07-14 16:06:46 +0900

    first init

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="myyrakle_test_module",
+    version="0.1.0",
+    license='MIT',
+    author="myyrakle",
+    author_email="sssang97@naver.com",
+    description="test module",
+    long_description=open('README.md').read(),
+    url="https://github.com/myyrakle/python_module_template",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
